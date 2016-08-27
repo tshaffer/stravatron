ae4d2a197a9d89cce323247585619fbd40e15bba
Ted Shaffer <ted@brightsign.biz>
2016-08-27 07:25:44 -0700

    created container for SummaryActivities

--- stravaClient/scripts/components/App.js
+++ stravaClient/scripts/components/App.js
@@ -1,57 +1,24 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import { loadSummaryActivities } from '../actions/index';
+import SummaryActivities from '../containers/summaryActivities';
 
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            foo: null
-        };
-    }
-
+export default class App extends Component {
 
     componentDidMount() {
         
         console.log("app.js::componentDidMount invoked");
-
-        this.props.loadSummaryActivities();
     }
-
-
+    
     render() {
-
-        // let athleteId = "unknown athlete";
-        // if (this.props.summaryActivities && this.props.summaryActivities.length > 0) {
-        //     debugger;
-        //     athleteId = this.props.summaryActivities[0].athlete.id.toString();
-        // }
 
         return (
             <MuiThemeProvider>
-                <div>
-                    pizza
-                    number of summary activities is: {Object.keys(this.props.summaryActivities.summaryActivitiesById).length}
-                </div>
+                <SummaryActivities />
             </MuiThemeProvider>
         );
     }
 }
 
-function mapStateToProps (state) {
-    return {
-        summaryActivities: state.summaryActivities
-    };
-}
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({loadSummaryActivities},
-        dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
 
---
+++ stravaClient/scripts/containers/summaryActivities.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { loadSummaryActivities } from '../actions/index';
+
+class SummaryActivities extends Component {
+
+    componentWillMount() {
+
+        console.log("app.js::componentWillMount invoked");
+
+        this.props.loadSummaryActivities();
+    }
+
+
+    render() {
+        return (
+            <div>
+                number of summary activities is: {Object.keys(this.props.summaryActivities.summaryActivitiesById).length}
+            </div>
+        );
+    }
+}
+
+function mapStateToProps (state) {
+    return {
+        summaryActivities: state.summaryActivities
+    };
+}
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({loadSummaryActivities},
+        dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SummaryActivities);
