0b360f93a400dce8eb94af92fefe210b9f347ccc
Ted Shaffer <ted@brightsign.biz>
2016-09-25 05:47:26 -0700

    select element to pick athlete

--- stravaClient/scripts/containers/landingContainer.js
+++ stravaClient/scripts/containers/landingContainer.js
@@ -26,10 +26,38 @@ class LandingContainer extends Component {
         });
     }
 
+    handleUpdateAthlete() {
+        console.log("update athlete");
+    }
+
     render() {
 
+        const self = this;
+
         console.log("number of athletes is:", this.props.athletes.length);
-        return <div>Pizza</div>;
+
+        if (this.props.athletes.length > 0) {
+
+            let selectOptions = this.props.athletes.map( (athlete) => {
+                return (
+                    <option value={athlete.name} key={athlete.stravaAthleteId}>{athlete.name}</option>
+                );
+            });
+
+            const athlete = this.props.athletes[0];
+
+            let athletesDropDown =
+                (<div>
+                    Select athlete:
+                    <select value={athlete.name} onChange={this.handleUpdateAthlete.bind(this)}>{selectOptions}</select>
+                </div>);
+
+            return athletesDropDown;
+
+        }
+        else {
+            return <div>Pizza</div>;
+        }
     }
 }
 
